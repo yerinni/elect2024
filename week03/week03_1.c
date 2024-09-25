ee674a73b569c8e2ff952627ccbba245008f61f3
yerinni <mintyerin0102@naver.com>
2024-09-25 13:45:18 +0900

    ㅇㅇ
    실습 1

---
+++ week03/week03_1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+// training 1 
+
+
+int main(void) {
+	int input_int;
+	float input_float;
+	
+	printf("enter an integer : ");
+	scanf("%d", &input_int);
+	
+	
+	printf("enter a float : ");
+	scanf("%f", &input_float);
+	
+	printf("integer : %d, float : %f\n", input_int, input_float);
+	
+	return 0;
+}
