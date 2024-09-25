5ab9ab502f17616f9681e5221b591b4dc67948b0
yerinni <mintyerin0102@naver.com>
2024-09-25 14:01:59 +0900

    week03_2 실습
    실습

---
+++ week03/2/week03_2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	char c;
+	int i;
+	
+	printf("input a number :");
+	scanf("%c", &c);
+	
+	i = c - '0';
+	printf("The input number is %i\n", i);
+	
+	return 0;
+    }
+
