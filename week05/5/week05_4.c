fdb841b9ee9e3c3fecac0d4f9ea070f672e110d5
yerinni <mintyerin0102@naver.com>
2024-10-02 14:07:49 +0900

    week05_5 실습

---
+++ week05/5/week05_4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main (void) {
+	int x, y;
+	int a, b, c, d, e;
+	printf("Input the integers: ");
+	scanf("%i %i", &x, &y);
+	
+	a = x & y;
+	b = x | y;
+	c = x ^ y;
+	d = x <<1; y;
+	e = x >>1; y;
+	
+	printf("& result is %d\n| result is %d\n^ result is %d\n<<1 result is %d\n>>1 result is %d", a, b, c, d, e);
+	
+}
