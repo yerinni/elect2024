5ac6c384a2aaa05550acc47675a71f02c6628bd8
yerinni <mintyerin0102@naver.com>
2024-10-16 13:44:21 +0900

    week07_3 실습

---
+++ week07/3/week07_3.c
@@ -0,0 +1,42 @@
+//week07_3
+//call by value
+
+/*
+void square(int a)
+{
+	a = a *a;
+}
+
+int main()
+{
+	int a = 2;
+	square(a);
+	printf("a=%i\n", a);
+}
+
+*/
+
+int square( int a )
+{
+	return(a *a);
+}
+
+int main()
+{
+	int a = 2;
+	a = square(a);
+	printf("a=%i\n", a);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
