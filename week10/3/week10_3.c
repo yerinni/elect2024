1d8cea8c3bba3c090246b990a8255af457f2fbf6
yerinni <mintyerin0102@naver.com>
2024-11-06 13:35:36 +0900

    week10_3 실습

--- week10/3/week10_3.c
+++ week10/3/week10_3.c
@@ -1,8 +1,15 @@
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #define SIZE 5
 
+
+
+
 int main(void)
+
+/*
 {
 	int i;
 	int grade[SIZE];
@@ -20,3 +27,43 @@ int main(void)
 		
 	return 0;
 }
+
+*/
+
+
+{
+	int i;
+	int a[SIZE] = {1,2,3,4,5};
+	int b[SIZE] = {1,2,3,7,10};
+	int flag_same = 1;
+	
+	
+	for (i=0;i<SIZE;i++)
+	{
+	if (a[i] != b[i])
+	{
+	  printf("%d", i);
+	  flag_same = 0;
+	}
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
