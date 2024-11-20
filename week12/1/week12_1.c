b54c742e165f5aeabdb765395c3850cc6b736a46
yerinni <mintyerin0102@naver.com>
2024-11-20 21:59:54 +0900

    week12_1

---
+++ week12/1/week12_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+	int i = 10;
+	char c = 'a';
+	
+	int *iptr = &i;
+	char *cptr = &c;
+	int *iptr2 = &i;
+	
+	printf("i : %p\n%p (size:%i)\n", iptr, &i, sizeof(iptr));
+	printf("c : %p\n%p (size:%i)\n", cptr, &c, sizeof(cptr));
+	printf("iptr2 : %p, %i\n", iptr2, *iptr2);
+	
+	return 0;
+}
