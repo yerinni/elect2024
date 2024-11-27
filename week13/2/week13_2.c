6f6c149bc333b37936c09b5c9627deb203643337
yerinni <mintyerin0102@naver.com>
2024-11-27 14:20:51 +0900

    week13_2 실습

---
+++ week13/2/week13_2.c
@@ -0,0 +1,31 @@
+//week13_2
+
+
+#include <stdio.h>
+#include <math.h>
+
+struct point {
+	int      x;
+	int      y;
+};
+
+int main (void) {
+	struct point p1, p2;
+	int xdiff, ydiff;
+	double dist;
+	
+	printf("input p1 coordinate (x y) :");
+	scanf("%d %d", &p1.x, &p1.y);
+	
+	printf("input p2 coordinate (x y) :");
+	scanf("%d %d", &p2.x, &p2.y);
+	
+	xdiff = p2.x - p1.x;
+	ydiff = p2.y - p1.y;
+	
+	dist = sqrt((xdiff * xdiff) + (ydiff * ydiff) );
+	
+	printf("distance: %f", dist );
+	
+
+}
